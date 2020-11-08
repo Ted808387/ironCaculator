818694c2323efbf26e855e66e3f9f05e9101f9ac
Ted808387 <58389259+Ted808387@users.noreply.github.com>
2020-11-08 15:39:54 +0800

    'add'

--- vue.confing.js
+++ vue.confing.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/iron-calculator-company/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/ironCaculator/' : '/',
   }
